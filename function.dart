8fdb547b0464f0e17b7c6fae8c51ae0d1e763b41
Rajnish <RajnishAryan12@gmail.com>
2023-11-04 17:42:53 +0530

    function in dart

---
+++ function.dart
@@ -0,0 +1,15 @@
+void main(){
+    var result=  multiplication(5,6);
+    print(result);
+    printmessage("how are you");
+}
+
+
+void printmessage(String message){
+    print("Hello Rajnish ${message} !");
+}
+
+int multiplication(int a, int b){
+    int sum= a+b;
+    return sum;
+}
