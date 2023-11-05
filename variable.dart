3c86cd1290966396bb0c2cc5354284f6366dc27e
Rajnish <RajnishAryan12@gmail.com>
2023-11-05 15:10:36 +0530

    variables in dart

---
+++ variable.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main(){
+  int price = 10;
+  String namee ="rajnish";
+  double a=10.2;
+  namee="raaj";
+  a=20.3;
+  price= 54;
+  var age = 20;
+  print("price is ${price} namee is ${namee} double is ${a} age is ${age}");
+
+  for (var i = 0; i < 200; i++) {
+    print(i);
+  }
+  print(sum(20, 30));
+
+}
+
+int sum(int a,int b){
+  return a+b;
+
+}
